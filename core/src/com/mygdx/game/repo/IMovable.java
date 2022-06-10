b99ef0066dddd29d2ffebb4be4a084a4f6c52f34
michal <miswistowski@gmail.com>
2022-06-10 12:02:23 +0200

    komentarze

--- core/src/com/mygdx/game/repo/IMovable.java
+++ core/src/com/mygdx/game/repo/IMovable.java
@@ -4,8 +4,18 @@ import com.badlogic.gdx.math.Vector2;
 
 public interface IMovable {
 
-
+     /**
+      * moves in a direction specified by vector
+      * @param vector direction of move
+      * @param delta delta time
+      */
      void move(Vector2 vector, float delta);
+
+     /**
+      * moves to a cartain spot on screen
+      * @param vector vector of position
+      * @param delta delta time
+      */
      void moveTo(Vector2 vector, float delta);
 
 }
